113e5b4aa246b78f649ce773b42cc7a8930da45b
TahaSait <148285176+TahaSait@users.noreply.github.com>
2023-11-06 11:10:17 -0700

    Added clacModule

---
+++ clacModule.py
@@ -0,0 +1,8 @@
+def add(num_1, num_2):
+    result = num_1 + num_2
+def subtract(num_1, num_2):
+        result = num_1 - num_2
+def multiply(num_1, num_2):
+    result = num_1 * num_2
+def divide(num_1, num_2):
+    result = num_1 / num_2
