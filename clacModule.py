b2fa2b6878f5a7e97c0cc0d05ecb32af22e7361a
TahaSait <148285176+TahaSait@users.noreply.github.com>
2023-11-06 11:16:39 -0700

    fixed functions

--- clacModule.py
+++ clacModule.py
@@ -1,8 +1,12 @@
 def add(num_1, num_2):
     result = num_1 + num_2
+    return result 
 def subtract(num_1, num_2):
-        result = num_1 - num_2
+    result = num_1 - num_2
+    return result
 def multiply(num_1, num_2):
     result = num_1 * num_2
+    return result
 def divide(num_1, num_2):
     result = num_1 / num_2
+    return result
